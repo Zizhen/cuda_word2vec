a3ec8a979b4b9fb502559d97523e796363e7eaf9
louis <liuzizhen1234@gmail.com>
2018-06-07 11:35:02 -0400

    vector manipulation module added

--- example.cu
+++ example.cu
@@ -113,16 +113,16 @@ int main(int argc, char* argv[]) {
     dim3 dimGrid(ceil(word_count/1024.0), 1, 1);
     dim3 dimBlock(1024, 1, 1);
     normalize<<<dimGrid, dimBlock>>>(matrix_d, normSum_d, matrixNorm_d, dim);
-    float *matRes = new float[matrix_size];
-    cudaMemcpy(matRes, matrixNorm_d, matrix_size*sizeof(float), cudaMemcpyDeviceToHost);
-
-    for(int j = 0; j < 150; j++){
-      cout << matRes[word2vec_map["king"]*150+j] << endl;
-    }
-    cout << endl;
-    for(int j = 0; j < 150; j++){
-      cout << matrix_h[word2vec_map["king"]*150+j] << endl;
-    }
+    // float *matRes = new float[matrix_size];
+    // cudaMemcpy(matRes, matrixNorm_d, matrix_size*sizeof(float), cudaMemcpyDeviceToHost);
+    //
+    // for(int j = 0; j < 150; j++){
+    //   cout << matRes[word2vec_map["king"]*150+j] << endl;
+    // }
+    // cout << endl;
+    // for(int j = 0; j < 150; j++){
+    //   cout << matrix_h[word2vec_map["king"]*150+j] << endl;
+    // }
 
     if(strcmp(argv[1],"analogy") == 0){
       if(argc == 7){
