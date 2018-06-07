7bc4d95c23e168e611a81df7410ce79efd0788fd
louis <liuzizhen1234@gmail.com>
2018-06-07 09:51:00 -0400

    vector manipulation module added

--- example.cu
+++ example.cu
@@ -29,10 +29,12 @@ void vectorManipulation(float* A, float* B, float* C, float* D, int len){
 }
 
 __global__
-void vecMatMultiplication(float* mat, float* vec, float* res, int len){
+void vecMatMultiplication(float* mat, float* vec, float* res, int len, int max){
   int i = threadIdx.x + blockDim.x * blockIdx.x;
-  for(int j = 0; j < len; j ++){
-    res[i] += mat[i*len+j] * vec[j];
+  if(i < max){
+    for(int j = 0; j < len; j ++){
+      res[i] += mat[i*len+j] * vec[j];
+    }
   }
 }
 
@@ -105,8 +107,8 @@ int main(int argc, char* argv[]) {
                   &matrix_d[idx_2*dim], &matrix_d[idx_3*dim], D, dim);
 
         dim3 dimGrid2(1, 1, 1);
-        dim3 dimBlock2(word_count, 1, 1);
-        vecMatMultiplication<<<dimGrid2, dimBlock2>>>(matrix_d, D, resVec_h, dim);
+        dim3 dimBlock2(ceil(word_count/256.0), 256, 1);
+        vecMatMultiplication<<<dimGrid2, dimBlock2>>>(matrix_d, D, resVec_h, dim, matrix_size);
 
         cudaMemcpy(resVec_h, D, word_count*sizeof(float), cudaMemcpyDeviceToHost);
         for(int i = 0; i < 20; i ++){
