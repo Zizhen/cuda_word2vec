e36cd64592f5f6fbfe96d7939315d931ad028399
louis <liuzizhen1234@gmail.com>
2018-06-07 09:02:17 -0400

    debugging cudaMalloc segmentation fault

--- example.cu
+++ example.cu
@@ -11,7 +11,6 @@ using namespace std;
 
 // Compile with:
 // nvcc -o example example.cu
-
 #define ERROR_CHECK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
@@ -22,12 +21,17 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
+__global__
+void vectorManipulation(float* A, float* B, float* C, float* D, int len){
+  int i = threadIdx.x + blockDim.x * blockIdx.x;
+  if (i < len) D[i] = A[i] + C[i] - B[i];
+}
 
 int main(int argc, char* argv[]) {
   if(argc == 2){
     if(strcmp(argv[1], "analogy") == 0){
-      cout << "usage: ./a.out analogy path/to/your/model dimension path/to/your/testfile" << endl;
-      cout << "   or: ./a.out analogy path/to/your/model dimension word1 word2 word3" << endl;      
+      cout << "usage: ./a.out analogy path/to/your/model dim path/to/your/testfile" << endl;
+      cout << "   or: ./a.out analogy path/to/your/model dim word1 word2 word3" << endl;
     }
     else
       cout << "function not supported" << endl;
@@ -43,15 +47,15 @@ int main(int argc, char* argv[]) {
     ifstream infile;
     infile.open(argv[2]);
     int word_count = 0;
-    int dimension = stod(argv[3]);
+    int dim = stod(argv[3]);
     while(getline(infile,str)){
       word_count++;
     }
     infile.close();
 
     infile.open(argv[2]);
-    int matrix_size = word_count*dimension;
-    float *word_matrix_h = new float[matrix_size];
+    int matrix_size = word_count*dim;
+    float *matrix_h = new float[matrix_size];
     int i = 0;
     while(getline(infile,str)){
       string buf;
@@ -60,27 +64,40 @@ int main(int argc, char* argv[]) {
       word2vec_map[buf] = i;
       int j = 0;
       while (ss >> buf){
-        word_matrix_h[i*dimension+j] = stod(buf);
+        matrix_h[i*dim+j] = stod(buf);
         j++;
       }
       i++;
     }
     infile.close();
-    float* word_matrix_d;
-    ERROR_CHECK(cudaMalloc((void **)&word_matrix_d, matrix_size*sizeof(float)));
-    cudaMemcpy(word_matrix_d, word_matrix_h, matrix_size*sizeof(float), cudaMemcpyHostToDevice);
+    float* matrix_d;
+    float* D;
+    cudaMalloc((void **)&matrix_d, matrix_size*sizeof(float));
+    cudaMalloc((void **)&D, dim*sizeof(float));
+    cudaMemcpy(matrix_d, matrix_h, matrix_size*sizeof(float), cudaMemcpyHostToDevice);
 
     if(strcmp(argv[1],"analogy") == 0){
       cout << "test" << endl;
       if(argc == 7){
+        int count[3];
+        for(int i = 0; i < 3; i++){
+          count[i] = word2vec_map.count(argv[4+i]);
+          if(count[i] != 1){
+              cout << "map does not contain the word: " << argv[4+i] << endl;
+              return;
+          }
+        }
         int idx_1 = word2vec_map[argv[4]];
         int idx_2 = word2vec_map[argv[5]];
         int idx_3 = word2vec_map[argv[6]];
-        cout << idx_1 << " " << idx_2 << " " << idx_3 << endl;
+        dim3 dimGrid(1, 1, 1);
+        dim3 dimBlock(dim, 1, 1);
+        vectorManipulation<<<dimGrid, dimBlock>>>(&matrix_d[idx_1*dim],
+                  &matrix_d[idx_2*dim], &matrix_d[idx_3*dim], D, dim);
       }
     }
 
-    cudaFree(word_matrix_d);
+    cudaFree(matrix_d);
   }
 
   return 0;
