6c811edd1e37ab4cb986c554303a4fe06872ae84
louis <liuzizhen1234@gmail.com>
2018-06-10 15:50:29 -0400

    cuBlas example

--- example.cu
+++ example.cu
@@ -114,19 +114,31 @@ int main(int argc, char* argv[]) {
     dim3 dimBlock(1024, 1, 1);
     normalize<<<dimGrid, dimBlock>>>(matrix_d, normSum_d, matrixNorm_d, dim);
 
-    float *matRes = new float[matrix_size];
-    cudaMemcpy(matRes, matrixNorm_d, matrix_size*sizeof(float), cudaMemcpyDeviceToHost);
     for(int j = 0; j < 150; j++){
-      cout << matRes[word2vec_map["king"]*150+j] << endl;
+      cout << matrix_h[word2vec_map["king"]*150+j] << endl;
     }
     cout << endl;
     for(int j = 0; j < 150; j++){
-      cout << matRes[word2vec_map["man"]*150+j] << endl;
+      cout << matrix_h[word2vec_map["man"]*150+j] << endl;
     }
     cout << endl;
     for(int j = 0; j < 150; j++){
-      cout << matRes[word2vec_map["woman"]*150+j] << endl;
+      cout << matrix_h[word2vec_map["woman"]*150+j] << endl;
     }
+
+    // float *matRes = new float[matrix_size];
+    // cudaMemcpy(matRes, matrixNorm_d, matrix_size*sizeof(float), cudaMemcpyDeviceToHost);
+    // for(int j = 0; j < 150; j++){
+    //   cout << matRes[word2vec_map["king"]*150+j] << endl;
+    // }
+    // cout << endl;
+    // for(int j = 0; j < 150; j++){
+    //   cout << matRes[word2vec_map["man"]*150+j] << endl;
+    // }
+    // cout << endl;
+    // for(int j = 0; j < 150; j++){
+    //   cout << matRes[word2vec_map["woman"]*150+j] << endl;
+    // }
 
     if(strcmp(argv[1],"analogy") == 0){
       if(argc == 7){
